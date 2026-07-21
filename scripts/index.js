7adfc378d6e4cc771bf677dbe5e072e329972d97
Salim Tufts <salimtufts9@gmail.com>
2026-07-21 00:56:33 -0700

    yes

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
 const container = document.querySelector(".about");
 const aboutText = document.querySelector("h2");
+const nav = document.querySelector("nav");
+nav.setAttribute("style", "translate: 0% -150%; ");
+
+setTimeout(() => {
+  nav.setAttribute("style", "translate: 0%");
+}, 600)
 
 
 setTimeout(() => {
     aboutText.setAttribute("style", "translate: 0%; transform: scale(1); opacity: 100%;");
+
 }, 1200);
 
 setTimeout(() => {
