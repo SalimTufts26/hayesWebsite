c9bcae193edec5fea4ebd5d59118d852bffa8935
Salim Tufts <salimtufts9@gmail.com>
2026-07-21 00:05:36 -0700

    adjusted max-height of .attn

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".about");
 const aboutText = document.querySelector("h2");
 
+
 setTimeout(() => {
     aboutText.setAttribute("style", "translate: 0%; transform: scale(1); opacity: 100%;");
 }, 500);
